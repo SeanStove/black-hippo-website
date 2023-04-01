045cd6a524303a6b091e7ee4ebebe9db3a58b20b
Sean Stove <sean.stove@gmail.com>
2023-04-01 09:32:45 +0200

    fixes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -59,13 +59,13 @@ export default function Home() {
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   viewBox="0 0 24 24"
-                  stroke-width="1.5"
+                  strokeWidth="1.5"
                   stroke="currentColor"
                   className="inline w-6 h-6 ml-2 text-white fill-current"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                     d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
                   />
                 </svg>
