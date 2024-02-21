6a789cac153b2645af5d795d24fa8f987864c4ad
Sean Stove <sean.stove@gmail.com>
2024-02-21 09:45:33 +0100

    plausible

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,17 +1,16 @@
 import { Head, Html, Main, NextScript } from "next/document";
 
 import React from "react";
-import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="nl" className="overflow-x-hidden scroll-smooth">
       <Head>
-        <Script
+        <script
           defer
           data-domain="blackhippo.be"
           src="https://plausible.io/js/script.js"
-        ></Script>
+        ></script>
         <meta property="og:title" content="Black hippo: Software development" />
         <meta property="og:url" content="https://blackhippo.be" />
         <meta
